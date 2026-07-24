aec9245e6a4abeb3b8e88ed58cc56903b675a3d3
TechforLearning4 <aigpt444@gmail.com>
2026-07-25 01:08:55 +0530

    Merge branch 'main' of

---
+++ pattern/butterfly.java
@@ -0,0 +1,33 @@
+public class butterfly {
+    public static void main(String[] args) {
+        int n = 4;
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int k = 1; k <= (2 * (n - i)); k++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int k = 1; k <= (2 * (n - i)); k++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
